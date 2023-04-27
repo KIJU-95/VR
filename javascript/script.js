530fd8b57b0ca25fc68a5207fd6c415f1b7c23ac
KIJU LEE <99025491+KIJU-95@users.noreply.github.com>
2023-04-27 09:58:16 +0900

    j

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,10 @@
 alert("홈페이지 작업 진행중입니다");
+
+
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
